2d9868a23479ad099daf6e578ceedbb6da6f58f1
M.D. Walters <popularvsofficial@gmail.com>
2023-05-03 17:20:47 +0000

    feat: list thingy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>SvelteRhythm</title>
+</svelte:head>
+
+<div class="m-3">
+    {#each new Array(20) as a}
+        <div class="p-5 mb-3 bg-violet-400 rounded text-white">
+            <h1 class="text-xl">Lorem ispum dolor sit amet</h1>
+            <p>Lorem ispum</p>
+        </div>
+    {/each}
+</div>
