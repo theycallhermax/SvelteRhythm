e59d4cf0e483aa5e30285223c27eff2f72806cd8
M.D. Walters <popularvsofficial@gmail.com>
2023-05-04 16:42:37 +0000

    fix: add white text by default

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="m-3">
     {#each new Array(20) as a}
-        <div class="p-5 mb-3 bg-violet-400 rounded text-white">
+        <div class="p-5 mb-3 bg-violet-400 rounded">
             <h1 class="text-xl">Lorem ispum dolor sit amet</h1>
             <p>Lorem ispum</p>
         </div>
