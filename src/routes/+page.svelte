39c12f24b4f59a9b4219c663cfce8aad1e9e1ecb
M.D. Walters <popularvsofficial@gmail.com>
2023-05-04 17:12:43 +0000

    feat: add bold text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,8 @@
 <div class="m-3">
     {#each new Array(20) as a}
         <div class="p-5 mb-3 bg-violet-400 rounded">
-            <h1 class="text-xl">Lorem ispum dolor sit amet</h1>
-            <p>Lorem ispum</p>
+            <h1 class="text-xl font-bold">Lorem ispum dolor sit amet</h1>
+            <p class="text-lg">Lorem ispum</p>
         </div>
     {/each}
 </div>
