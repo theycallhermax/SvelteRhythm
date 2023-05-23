7cb23e54cc282e0d64eb5c9e20581cd0edb33c97
M.D. Walters <popularvsofficial@gmail.com>
2023-05-23 17:20:10 +0000

    h

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<div class="m-3">
+    <div class="p-5 mb-3 bg-violet-400 rounded max-h-screen">
+        <p class="text-xl font-bold">{$page.error.message}</p>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="m-3">
     {#each new Array(20) as a}
-        <div class="p-5 mb-3 bg-violet-400 rounded">
+        <div class="p-5 mb-3 bg-violet-400 rounded drop-shadow-md hover:drop-shadow-xl hover:scale-110 hover:p-20 hover:pt-5 hover:pb-5 transition ease-in-out">
             <h1 class="text-xl font-bold">Lorem ispum dolor sit amet</h1>
             <p class="text-lg">Lorem ispum</p>
         </div>
